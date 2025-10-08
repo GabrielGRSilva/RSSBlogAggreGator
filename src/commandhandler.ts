463370c76e8f65abe04b6d346a53a0e886a2c58e
Gabriel G. R. Silva <gabrielgrsilva@outlook.com.br>
2025-10-08 11:44:41 -0300

    Add handler helper functions

--- src/commandhandler.ts
+++ src/commandhandler.ts
@@ -1,8 +1,13 @@
 import {setUser} from './config.js';
 
-type CommandHandler = (cmdName: string, ...args: string[]) => void;
+export type CommandHandler = (cmdName: string, ...args: string[]) => void;
 
-function handlerLogin(cmdName: string, ...args: string[]) {
+export type CommandsRegistry = { //Type to hold available commands
+    name: string[],
+    handler: CommandHandler[]
+};
+
+export function handlerLogin(cmdName: string, ...args: string[]){
     if (!args || args.length > 1){
         throw new Error('You need to login using a username! login [USERNAME]');
     };
@@ -10,4 +15,21 @@ function handlerLogin(cmdName: string, ...args: string[]) {
     setUser(args[0]); //Sets the username
 
     console.log(`Username ${args[0]} has been set!`);
+};
+
+export function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler){ //This function registers a new handler function for a command name.
+    registry.name.push(cmdName);
+    registry.handler.push(handler);
+};
+
+export function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]){ //This function runs a given command with the provided state if it exists.
+    for (let cmd of registry.name){
+        if (cmdName == cmd){
+            const foundHandler = registry.handler[registry.name.indexOf(cmd)];
+
+            foundHandler(cmdName, ...args);
+            return;
+        };
+    };
+    throw new Error('Command not found!');
 };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import {setUser, readConfig} from './config.js';
+import * as ch from './commandhandler.js';
+import { argv } from 'node:process';
 
 function main() {
-    setUser('Gabriel Silva');
-    console.log(readConfig());
+    const registry: ch.CommandsRegistry = {
+        name: [],
+        handler: []
+    };
+
+    ch.registerCommand(registry, 'login', ch.handlerLogin);
+    const userInputs = process.argv;
+
+    if (userInputs.length < 2){
+        console.log("No commands were given!");
+        process.exit(1);
+    };
+
+    const cmd = userInputs[2];
+    const args = userInputs.slice(3)
 };
 
 main();
